2d18f8d1aed6461944a6aac996869b8ed6d3075a
J3niff3r Guañuna <jeny.guanuna@gmail.com>
2021-08-13 01:59:28 -0500

    routes

--- routes/api/v1/authentication.php
+++ routes/api/v1/authentication.php
@@ -13,7 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
 Route::apiResource('users', UserController::class);
